e15f504fd9e37ba8096abe5f6c1a27cc121b5e04
SarveshKulkarni1111 <109644906+SarveshKulkarni1111@users.noreply.github.com>
2024-03-07 12:41:55 +0530

    Added sticky navbar and corrected some css issues

--- src/navbar.js
+++ src/navbar.js
@@ -1,17 +1,15 @@
 import React, { useState } from 'react';
 import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
-import {Link} from "react-router-dom";
+import { NavLink, useLocation } from 'react-router-dom';
 
 const Navbar = ({ onSearch }) => {
-  // State to manage the navbar's visibility
   const [nav, setNav] = useState(false);
   const [searchQuery, setSearchQuery] = useState('');
+  const location = useLocation();
 
-  // Toggle function to handle the navbar's display
   const handleNav = () => {
     setNav(!nav);
   };
-
 
   const handleSearch = (event) => {
     const query = event.target.value;
@@ -19,7 +17,6 @@ const Navbar = ({ onSearch }) => {
     onSearch(query);
   };
 
-  // Array containing navigation items
   const navItems = [
     { id: 1, text: 'Home', path: '/' },
     { id: 2, text: 'Business', path: '/business' },
@@ -31,55 +28,42 @@ const Navbar = ({ onSearch }) => {
   ];
 
   return (
-    <div className= 'flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-black'>
-      {/* Logo */}
-      <Link to="/"><h1 className=' text-3xl font-bold text-black p-6'>NewsNow</h1></Link>
+    <div className='sticky top-0 z-50 bg-white'>
+      <div className='flex justify-between items-center max-w-[1240px] mx-auto px-4 text-black'>
+        <NavLink to='/'><h1 className='text-3xl font-bold text-black p-6'>NewsNow</h1></NavLink>
 
-      <input
-                  type="search"
-                  className="relative m-0 block flex-auto rounded border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
-                  value={searchQuery}
-                  onChange={handleSearch}
-                  placeholder="Search"
-                  aria-label="Search"
-                  aria-describedby="button-addon2" />
+        <input
+          type='search'
+          className='relative m-0 block flex-auto rounded border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary'
+          value={searchQuery}
+          onChange={handleSearch}
+          placeholder='Search'
+          aria-label='Search'
+          aria-describedby='button-addon2'
+        />
 
-              
-              <span
-                  className="input-group-text flex items-center whitespace-nowrap rounded px-3 py-1.5 text-center text-base font-normal text-neutral-700 dark:text-neutral-200"
-                  id="basic-addon2">
-                  <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                      className="h-5 w-5">
-                      <path
-                          fillRule="evenodd"
-                          d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
-                          clipRule="evenodd" />
-                  </svg>
-              </span>
+        <span className='input-group-text flex items-center whitespace-nowrap rounded px-3 py-1.5 text-center text-base font-normal text-neutral-700 dark:text-neutral-200' id='basic-addon2'>
+          <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' fill='currentColor' className='h-5 w-5'>
+            <path fillRule='evenodd' d='M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z' clipRule='evenodd' />
+          </svg>
+        </span>
 
-      {/* Desktop Navigation */}
-      <ul className='hidden md:flex'>
-        {navItems.map(item => (
-          <li
-            key={item.id}
-            className='p-4 hover:bg-[#00df9a] rounded-xl m-2 cursor-pointer duration-300 hover:text-black'
-          >
-             <Link to={item.path}>{item.text}</Link>
-          </li>
-        ))}
-      </ul>
+        <ul className='hidden md:flex'>
+          {navItems.map(item => (
+            <li
+              key={item.id}
+              className={`p-4 hover:bg-[#00df9a] rounded-xl m-2 cursor-pointer duration-300 hover:text-black ${location.pathname === item.path ? 'underline' : ''}`}
+            >
+              <NavLink to={item.path}>{item.text}</NavLink>
+            </li>
+          ))}
+        </ul>
 
-
-
-      {/* Mobile Navigation Icon */}
-      <div onClick={handleNav} className='block md:hidden'>
-        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+        <div onClick={handleNav} className='block md:hidden'>
+          {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+        </div>
       </div>
 
-      {/* Mobile Navigation Menu */}
       <ul
         className={
           nav
@@ -87,16 +71,14 @@ const Navbar = ({ onSearch }) => {
             : 'ease-in-out w-[60%] duration-500 fixed top-0 bottom-0 left-[-100%]'
         }
       >
-      
         <h1 className='w-full text-3xl font-bold text-black m-4'>News</h1>
 
-        
         {navItems.map(item => (
           <li
             key={item.id}
-            className='p-4 border-b rounded-xl hover:bg-[#00df9a] duration-300 hover:text-black cursor-pointer border-gray-600'
+            className={`p-4 border-b rounded-xl hover:bg-[#00df9a] duration-300 hover:text-black cursor-pointer border-gray-600 ${location.pathname === item.path ? 'underline' : ''}`}
           >
-             <Link to={item.path}>{item.text}</Link>
+            <NavLink to={item.path}>{item.text}</NavLink>
           </li>
         ))}
       </ul>
